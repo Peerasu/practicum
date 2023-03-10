6566bcb4c44b9590f42fa828f562409fa390f6a0
Peerasu <fecownp@gmail.com>
2023-03-10 17:55:45 +0700

    fix bug6

--- light-3/light-3.c
+++ light-3/light-3.c
@@ -9,11 +9,11 @@ void main() {
 
     while (1) {
         light = read_adc(PC4);
-        if (light < 350) {
+        if (light < 250) {
             set_led_value(0b001);
             _delay_ms(100);
         }
-        else if (light >= 350 && light < 500) {
+        else if (light >= 250 && light < 500) {
             set_led_value(0b010);
             _delay_ms(100);
         }
