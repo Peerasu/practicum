0647f1f91037a7f1ba88609c9b62ea92f3ad4eac
Peerasu <fecownp@gmail.com>
2023-03-10 16:59:22 +0700

    yay

---
+++ count-sw/count-sw.c
@@ -0,0 +1,19 @@
+#define F_CPU 16000000
+#include <avr/io.h>
+#include <util/delay.h>
+#include "../peri.h"
+
+void main() {
+    uint8_t count = 0;
+    init_peri();
+    set_led_value(0);
+
+    while(1) {
+        if (SWITCH_PRESSED) {
+            count++;
+        }
+        count = count%8;
+
+        set_led_value(count);
+    }
+}
