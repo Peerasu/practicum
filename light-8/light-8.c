c8a60abc3103aca436d911efb1cfbede9c823904
Peerasu <fecownp@gmail.com>
2023-03-10 19:34:21 +0700

    fix bug7

--- light-8/light-8.c
+++ light-8/light-8.c
@@ -6,38 +6,37 @@
 void main() {
     uint16_t light;
     init_peri();
-    int max = 0, min = 99999;
+    int min = 600;
+    int max = 1023;
 
     while (1) {
         light = read_adc(PC4);
-        if (light > max) max = light;
-        if (light < min) min = light;
 
-        if (light < min+0.3*max) {
+        if (light < min) {
             set_led_value(0b000);
             _delay_ms(100);
         }
-        else if (light >= min+0.3*max && light < min+0.475*max) {
+        else if (light >= min+50 && light < min +100) {
             set_led_value(0b001);
             _delay_ms(100);
         }
-        else if (light >= min+0.475*max && light < min+0.550*max) {
+        else if (light >= min+100 && light < min+150) {
             set_led_value(0b010);
             _delay_ms(100);
         }
-        else if (light >= min+0.550*max && light < min+0.625*max) {
+        else if (light >= min+150 && light < min+200) {
             set_led_value(0b011);
             _delay_ms(100);
         }
-        else if (light >= min+0.625*max && light < min+0.7*max) {
+        else if (light >= min+200 && light < min+250) {
             set_led_value(0b100);
             _delay_ms(100);
         }
-        else if (light >= min+0.7*max && light < min+0.775*max) {
+        else if (light >= min+250 && light < min+300) {
             set_led_value(0b101);
             _delay_ms(100);
         }
-        else if (light >= min+0.775*max && light < min+0.85*max) {
+        else if (light >= min+300 && light < min+350) {
             set_led_value(0b110);
             _delay_ms(100);
         }
